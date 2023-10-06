e118a1aa8132b2851392a779942ff6e269f4131c
Honeymishra123 <145647499+Honeymishra123@users.noreply.github.com>
2023-10-06 16:43:57 +0530

    Add files via upload

---
+++ Table.java
@@ -0,0 +1,13 @@
+package firstOne;
+
+public class Table {
+
+	public static void main(String[] args) {
+		int n=5;
+		for(int i=0;i<11;i++) {
+			System.out.println(n+" * "+i+"="+i*n);
+		}
+		
+	}
+
+}
